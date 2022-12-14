4f1d817abc8955d80a62e884ceac1847b5f49fc2
Daniel Hsu <daniel2007704@gmail.com>
2022-12-15 05:56:43 +0800

    finish 1st part: 5% transfer fee will be distributed to all the holders immediately

--- contracts/FeeSharingERC20.sol
+++ contracts/FeeSharingERC20.sol
@@ -3,18 +3,29 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol"; // nonReentrant
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; // nonReentrant
 
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2ERC20.sol';
 
 contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
     mapping(address => uint256) private _internalTokenBalances;   // the balance for reflection token (internal)
 
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    mapping (address => bool) private _isExcludedFromPayingFee;
+    mapping(address => bool) private _isExcludedFromPayingFee;
+
+    address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant BUSD = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
+
+    IUniswapV2Router02 public uniswapV2Router02;
+    IUniswapV2Factory public uniswapV2Factory;
+    address public busdPairAddress;
+
 
     uint256 private constant MAX = type(uint).max;
-    uint256 private _totalSupply = 1e10 * 1e18;
+    uint256 private _totalSupply = 1e5 * 1e18; // total supply: 100,000
     uint256 private _internalTokenTotalSupply = (MAX / _totalSupply) * _totalSupply;
     // let (_internalTokenTotalSupply % _totalSupply) == 0
     // --> (_internalTokenTotalSupply / _totalSupply): # of internal tokens that 1 of the ERC-20 token represent
@@ -24,15 +35,18 @@ contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
     uint8 private _decimals = 18;
 
     // Transfer tax: 5% (the fee will be distributed to other holders immeditately when the transfer happens)
-    uint8 transferTaxNumerator = 5;
-    uint8 transferTaxDenominator = 100;
+    //   5% = 1/20
+    uint8 transferTaxNumerator = 1; 
+    uint8 transferTaxDenominator = 20;
     // Uniswap sell tax: 10% 
     //   - 5% for adding liquidity into uniswap pool
+    //     5% = 1 / 20
     //   - 5% will be distributed to the token stakers
-    uint8 sellTaxForAddingLiquidityNumerator = 5;
-    uint8 sellTaxForAddingLiquidityDenominator = 100;
-    uint8 sellTaxForStakerRewardNumerator = 5;
-    uint8 sellTaxForStakerRewardDenominator = 100;
+    //     5% = 1 / 20
+    uint8 sellTaxForAddingLiquidityNumerator = 1;
+    uint8 sellTaxForAddingLiquidityDenominator = 20;
+    uint8 sellTaxForStakerRewardNumerator = 1;
+    uint8 sellTaxForStakerRewardDenominator = 20;
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
@@ -41,6 +55,12 @@ contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
         _internalTokenBalances[owner()] = _internalTokenTotalSupply;
         _isExcludedFromPayingFee[owner()] = true;
         _isExcludedFromPayingFee[address(this)] = true;
+
+        // uniswapV2Router02 = IUniswapV2Router02(ROUTER);
+        // uniswapV2Factory = IUniswapV2Factory(uniswapV2Router02.factory());
+        // busdPairAddress = uniswapV2Factory.createPair(address(this), BUSD);
+
+        emit Transfer(address(0), _msgSender(), _totalSupply);
     }
 
     // return # of internal tokens that 1 of the ERC-20 token represent
@@ -60,7 +80,7 @@ contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
         return _decimals;
     }
 
-    function balanceOf(address account) external view override returns (uint256) {
+    function balanceOf(address account) public view override returns (uint256) {
         uint256 internalTokenBalance = _internalTokenBalances[account];
         uint256 internalTokenAmountFor1ERC20 = _getInternalTokenExchangeRateForERC20Token();
         
@@ -72,11 +92,11 @@ contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
     }
 
     function transfer(address to, uint256 amount) external returns (bool) {        
-        _transfer(_msgSender(), recipient, amount);
+        _transfer(_msgSender(), to, amount);
         return true;
     }
 
-    function allowance(address owner, address spender) external view returns (uint256) {
+    function allowance(address owner, address spender) public view returns (uint256) {
         return _allowances[owner][spender];
     }
 
@@ -89,6 +109,14 @@ contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
         _spendAllowance(from, _msgSender(), amount);
         _transfer(from, to, amount);
         return true;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
     }
 
     function _spendAllowance(address owner, address spender, uint256 amount) internal virtual {
@@ -106,26 +134,58 @@ contract FeeSharingERC20 is IERC20, Ownable, ReentrancyGuard {
         require(to != address(0), "ERC20: transfer to the zero address");
 
         require(balanceOf(from) >= amount, "ERC20: transfer amount exceeds balance");
+        require(amount > 0, "Transfer amount must be greater than zero");
 
         uint256 internalTokenAmountFor1ERC20 = _getInternalTokenExchangeRateForERC20Token();
+    
+        uint256 transferTax;
+        uint256 transferAmountAfterTax;
+        uint256 sellTaxForAddingLiquidity;
+    
+        uint256 internalTokenTransferTax;
+        uint256 internalTokenTransferAmountAfterTax;
+        uint256 internalTokenSellTaxForAddingLiquidity;
 
-
-        uint256 fromBalance = _balances[from];
-        require(fromBalance >= amount, "");
-        unchecked {
-            _balances[from] = fromBalance - amount;
-            // Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is preserved by
-            // decrementing then incrementing.
-            _balances[to] += amount;
+        if (_isExcludedFromPayingFee[from]) {
+            transferTax = 0;
         }
+        else {
+        // else if (to != busdPairAddress) {
+            transferTax = amount / transferTaxDenominator * transferTaxNumerator;
+        }
+        // else {
+            // This situation is selling the token on uniswap
+            // transferTax = 0;
+            // sellTaxForAddingLiquidity = amount / sellTaxForAddingLiquidityDenominator * sellTaxForAddingLiquidityNumerator;
 
-        emit Transfer(from, to, amount);
+            // addLiquidityForBUSDPair(sellTaxForAddingLiquidity, 2 * sellTaxForAddingLiquidity);
+            // amount -= sellTaxForAddingLiquidity;
+        // }
+        
+        transferAmountAfterTax = amount - transferTax;
 
-        _afterTokenTransfer(from, to, amount);
+        internalTokenTransferTax = transferTax * internalTokenAmountFor1ERC20;
+        internalTokenTransferAmountAfterTax = transferAmountAfterTax * internalTokenAmountFor1ERC20;
+
+        _internalTokenBalances[from] -= (internalTokenTransferTax + internalTokenTransferAmountAfterTax);
+        _internalTokenBalances[to] += internalTokenTransferAmountAfterTax;
+        _internalTokenTotalSupply -= internalTokenTransferTax;
+
+        emit Transfer(from, to, transferAmountAfterTax);
     }
 
-
-
+    function addLiquidityForBUSDPair(uint tokenAmount, uint BUSDAmount) internal {
+        (uint amountA, uint amountB, uint liquidity) = uniswapV2Router02.addLiquidity(
+            BUSD,
+            address(this),
+            BUSDAmount,
+            tokenAmount,
+            1,
+            1,
+            address(this),
+            block.timestamp
+        );
+    }
 }
 
 
--- test/test.js
+++ test/test.js
@@ -5,25 +5,109 @@ const { expect } = require("chai");
 // Close warning: Duplicate definitions
 Logger.setLogLevel(LogLevel.ERROR);
 
-describe("", function () {
+async function deployContracts() {
+    const feeSharingERC20Factory = await ethers.getContractFactory("FeeSharingERC20");
+    const feeSharingERC20 = await feeSharingERC20Factory.deploy("Fee Sharing Token", "FST");
+    await feeSharingERC20.deployed();
+    
+    return {
+        feeSharingERC20
+    };
+}
+
+
+describe("FeeSharingERC20", function () {
     let owner; // owner who deploys all contracts
-    let user1, user2, user3;
+    let user1, user2, user3, user4, user5;
 
-    async function deployContracts() {
-        return {
-            
-        };
-    }
+    let feeSharingERC20;
 
+    const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    // address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    // address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    // address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    const EPSILON = 1;
+
+    const TOTAL_SUPPLY = ethers.utils.parseUnits("100000", 18); // 1e5
+
+    const USER1_INITIAL_TOKEN_AMOUNT = ethers.utils.parseUnits("10000", 18);
+    const USER2_INITIAL_TOKEN_AMOUNT = ethers.utils.parseUnits("20000", 18);
+    const USER3_INITIAL_TOKEN_AMOUNT = ethers.utils.parseUnits("30000", 18);
+    const USER4_INITIAL_TOKEN_AMOUNT = ethers.utils.parseUnits("40000", 18);
+    const USER5_INITIAL_TOKEN_AMOUNT = 0;
+
+    const USER4_TRANSFER_TO_USER5_TOKEN_AMOUNT = ethers.utils.parseUnits("10000", 18);
+    
     before(async () => {
-        [owner, user1, user2, user3] = await ethers.getSigners();
+        [owner, user1, user2, user3, user4, user5] = await ethers.getSigners();
     });
 
-    it("", async function () {
-        const {
-            
-        } = await loadFixture(initializeTokenStatus);
-
+    it("Deploy the feeSharingERC20 contract, owner should have 1e10 tokens initially", async function () {
+        ({ feeSharingERC20 } = await loadFixture(deployContracts));
+        expect(await feeSharingERC20.balanceOf(owner.address)).to.equal(TOTAL_SUPPLY);
     });
+
+    it("Contract owner transfer 100, 200, 300, 400 tokens to user1, user2, user3, user4, respectively. The transfer from contract owner should not be taxed", async function () {
+        await feeSharingERC20.connect(owner).transfer(user1.address, USER1_INITIAL_TOKEN_AMOUNT);
+        await feeSharingERC20.connect(owner).transfer(user2.address, USER2_INITIAL_TOKEN_AMOUNT);
+        await feeSharingERC20.connect(owner).transfer(user3.address, USER3_INITIAL_TOKEN_AMOUNT);
+        await feeSharingERC20.connect(owner).transfer(user4.address, USER4_INITIAL_TOKEN_AMOUNT);
+
+        expect(await feeSharingERC20.balanceOf(user1.address)).to.equal(USER1_INITIAL_TOKEN_AMOUNT);
+        expect(await feeSharingERC20.balanceOf(user2.address)).to.equal(USER2_INITIAL_TOKEN_AMOUNT);
+        expect(await feeSharingERC20.balanceOf(user3.address)).to.equal(USER3_INITIAL_TOKEN_AMOUNT);
+        expect(await feeSharingERC20.balanceOf(user4.address)).to.equal(USER4_INITIAL_TOKEN_AMOUNT);
+        expect(await feeSharingERC20.balanceOf(user5.address)).to.equal(USER5_INITIAL_TOKEN_AMOUNT); // user5: 0 tokens
+    });
+
+    it("User4 transfer 10000 tokens to user5, the tax should be 500 tokens and distributed to all holders", async function () {
+        // Initial Balance:
+        //  User1: 10000
+        //  User1: 20000
+        //  User1: 30000
+        //  User1: 40000
+        //  User5: 0
+
+        await feeSharingERC20.connect(user4).transfer(user5.address, USER4_TRANSFER_TO_USER5_TOKEN_AMOUNT);
+        
+        const user1Balance = await feeSharingERC20.balanceOf(user1.address) / 1e18;
+        const user2Balance = await feeSharingERC20.balanceOf(user2.address) / 1e18;
+        const user3Balance = await feeSharingERC20.balanceOf(user3.address) / 1e18;
+        const user4Balance = await feeSharingERC20.balanceOf(user4.address) / 1e18;
+        const user5Balance = await feeSharingERC20.balanceOf(user5.address) / 1e18;
+
+        // console.log("User1: " + `${user1Balance}`);
+        // console.log("User2: " + `${user2Balance}`);
+        // console.log("User3: " + `${user3Balance}`);
+        // console.log("User4: " + `${user4Balance}`);
+        // console.log("User5: " + `${user5Balance}`);
+
+        // The tax (500 tokens) should be distributed to all the holders:
+        //   User1: 10000 + (10000 / 100000) * 500 = 10050 tokens
+        //   User2: 20000 + (20000 / 100000) * 500 = 20100 tokens
+        //   User3: 30000 + (30000 / 100000) * 500 = 30150 tokens
+        //   User4: 40000 - 10000 + (30000 / 100000) * 500 = 30150 tokens
+        //   User5: 0 + 9500 + (9500 / 100000) * 500 = 9547.5 tokens
+
+        expect(Math.abs(user1Balance - 10050)).to.be.lessThan(EPSILON);
+        expect(Math.abs(user2Balance - 20100)).to.be.lessThan(EPSILON);
+        expect(Math.abs(user3Balance - 30150)).to.be.lessThan(EPSILON);
+        expect(Math.abs(user4Balance - 30150)).to.be.lessThan(EPSILON);
+        expect(Math.abs(user5Balance - 9547.5)).to.be.lessThan(EPSILON);
+    });
+    
+    // it("Deploy the feeSharingERC20 contract, owner should have 1e10 tokens initially", async function () {
+        
+    // });
+
+    // it("Deploy the feeSharingERC20 contract, owner should have 1e10 tokens initially", async function () {
+        
+    // });
+    
+    // it("Deploy the feeSharingERC20 contract, owner should have 1e10 tokens initially", async function () {
+        
+    // });
+
 
 });
