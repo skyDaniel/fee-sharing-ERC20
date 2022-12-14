b7a2428a36217c99421bd6a795f2ae550e6e3e8c
Daniel Hsu <daniel2007704@gmail.com>
2022-12-14 22:52:38 +0800

    update

--- test/test.js
+++ test/test.js
@@ -5,19 +5,13 @@ const { expect } = require("chai");
 // Close warning: Duplicate definitions
 Logger.setLogLevel(LogLevel.ERROR);
 
-describe("CERC20 - Should be able to borrow/repay", function () {
+describe("", function () {
     let owner; // owner who deploys all contracts
     let user1, user2, user3;
 
     async function deployContracts() {
         return {
-            comptroller,
-            priceOracle,
-            erc20TokenA,
-            erc20TokenB,
-            interestRateModel,
-            cErc20TokenA,
-            cErc20TokenB
+            
         };
     }
 
@@ -25,15 +19,9 @@ describe("CERC20 - Should be able to borrow/repay", function () {
         [owner, user1, user2, user3] = await ethers.getSigners();
     });
 
-    it("User1: Borrow 50 tokenA using 1 tokenB as the collateral, and then repay 50 tokenA to the pool", async function () {
+    it("", async function () {
         const {
-            comptroller, 
-            priceOracle, 
-            erc20TokenA, 
-            erc20TokenB, 
-            interestRateModel, 
-            cErc20TokenA, 
-            cErc20TokenB 
+            
         } = await loadFixture(initializeTokenStatus);
 
     });
